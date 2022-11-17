f7d00baec05773ecefb19ba9a120ab3d876db997
Hyejeong Park <effie.thedev@gmail.com>
2022-11-17 20:52:26 +0900

    Initial Commit

---
+++ QueueTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueueTicket
+//
+//  Created by Hyejeong Park on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
